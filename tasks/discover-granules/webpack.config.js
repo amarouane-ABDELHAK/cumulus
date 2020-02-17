5e2c4b89620b0816623feb2a4fbca610dd8a7890
Mark Boyd <markdavidboyd@gmail.com>
2020-02-17 12:14:32 -0500

    experiment with using jsdom for HTTP file discovery

--- tasks/discover-granules/webpack.config.js
+++ tasks/discover-granules/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 // path to module root
 const root = path.resolve(__dirname);
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: process.env.PRODUCTION ? 'production' : 'development',
@@ -15,25 +16,62 @@ module.exports = {
     }
   },
   externals: [
-    'aws-sdk',
-    'electron',
-    'canvas',
-    {'formidable': 'url'}
+    {
+      'aws-sdk': true,
+      'electron': true,
+      'formidable': 'url'
+      // '../build/Release/canvas.node': true
+    },
+    function(context, request, callback) {
+      if (/^..\/build\/Release\/canvas.node$/.test(request)){
+        console.log(request);
+        const test = './build/Release/canvas.node';
+        return callback(null, 'commonjs ' + test);
+      }
+      callback();
+    }
+  ],
+  resolve: {
+    alias: {
+      '../build/Release/canvas.node': './build/Release/canvas.node'
+    }
+  },
+  plugins: [
+    new CopyPlugin([{
+      from: 'node_modules/@cumulus/ingest/node_modules/canvas/build/Release/canvas.node',
+      to: 'build/Release'
+    }])
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
+        exclude: /node_modules\/(?!(jsdom)\/).*/,
         use: [
           {
             loader: 'babel-loader',
             options: {
-              cacheDirectory: true
-            },
-          },
-        ],
+              cacheDirectory: true,
+              presets: [
+                ["@babel/preset-env", {
+                  modules: "commonjs",
+                  targets: {
+                    node: "10"
+                  }
+                }]
+              ],
+              plugins: [
+                "@babel/plugin-proposal-optional-catch-binding"
+              ]
+            }
+          }
+        ]
       },
+      {
+        test: /\.node$/,
+        use: 'node-loader'
+      }
     ],
   },
   devtool: 'inline-source-map',
